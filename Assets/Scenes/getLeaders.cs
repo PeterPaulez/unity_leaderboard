936a05b0b78567a0bccd4778e449f304b3d95cb5
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-04-25 21:06:40 +0200

    CHANGE: Cleaning C# script

--- Assets/Scenes/getLeaders.cs
+++ Assets/Scenes/getLeaders.cs
@@ -14,16 +14,14 @@ public class getLeaders : MonoBehaviour
     // Start is called before the first frame update
     async System.Threading.Tasks.Task Start()
     {
-        Debug.Log("Hola mundo Acción");
         HttpClient client = new HttpClient();
         client.BaseAddress = new System.Uri(URL);
 
-        // Add an Accept header for JSON format.
         client.DefaultRequestHeaders.Accept.Add(
         new MediaTypeWithQualityHeaderValue("application/json"));
 
         // List data response.
-        HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+        HttpResponseMessage response = client.GetAsync(urlParameters).Result;  
         if (response.IsSuccessStatusCode)
         {
             // Parse the response body.
@@ -34,13 +32,7 @@ public class getLeaders : MonoBehaviour
             var dataObjects = JsonConvert.DeserializeObject(customerJsonString);
             Debug.Log(customerJsonString);
 
-            /*
-            foreach (int i in dataObjects.Results)
-            {
-                Debug.Log("{0}", i);
-            }
-            */
-            
+            // LeaderBoard en customerJsonString.ranking
         }
         else
         {
@@ -48,10 +40,6 @@ public class getLeaders : MonoBehaviour
             Debug.Log(response.StatusCode);
             Debug.Log(response.ReasonPhrase);
         }
-
-        //Make any other calls using HttpClient here.
-
-        //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
         client.Dispose();
     } 
 
